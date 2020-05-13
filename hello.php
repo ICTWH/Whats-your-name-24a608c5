d9d54a9713998a16439b8a8117651531bf4cb3d9
Wingho Tang <2086640@talnet.nl>
2020-05-13 16:13:14 +0200

    Uitwerking van de exercise

---
+++ hello.php
@@ -0,0 +1,20 @@
+<?php 
+
+$voornaam = "Luca";
+$achternaam= "de Vries";
+$break= '. PHP_EOL .';
+
+echo "Hallo gebruiker!";
+
+
+$line = readline( "gebruiker zegt:");
+
+  
+print ("$line: Mijn voornaam is:" "$voornaam" . PHP_EOL .);
+
+echo ("$line: Mijn achternaam is: $achternaam"  . PHP_EOL .);
+
+echo ("$line: Mijn naam is: $voornaam $achternaam"  . PHP_EOL .);
+
+?>
+  
